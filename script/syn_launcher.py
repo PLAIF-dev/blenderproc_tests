96b0a86263db2b85b57aa818c1ce7f5eb3e78f48
jslee <jslee@plaif.com>
2023-07-17 20:06:59 +0900

    feat: fit coding style to pylint standard

--- script/syn_launcher.py
+++ script/syn_launcher.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
+'''synthetic launcher docstr'''
 
+import os
+import time
+import json
 import rospy
 from std_msgs.msg import String
-import os
 import sensor_msgs
-from PIL import Image as I
 from sensor_msgs.msg import Image
 import numpy as np
 from std_msgs.msg import Header
-import time
-import json
 
 def rgb_to_msg(img):
+    '''convert rgb image to msg'''
+
     # Create a ROS1 Image message
     ros_msg = Image()
-    ros_msg.header = Header(
-        stamp=rospy.Time.now()
-    )  # Set the header with current time
+    ros_msg.header = Header(stamp=rospy.Time.now())  # Set the header with current time
     ros_msg.height = img.shape[0]  # Set image height
     ros_msg.width = img.shape[1]  # Set image width
-    ros_msg.encoding = (
-        "rgb8"  # Set encoding as 32-bit floating point (single channel)
-    )
+    ros_msg.encoding = "rgb8"  # Set encoding as 32-bit floating point (single channel)
     ros_msg.is_bigendian = False  # Set endianness
     ros_msg.step = ros_msg.width * 3  # Set step size (4 bytes per float)
 
@@ -30,63 +28,77 @@ def rgb_to_msg(img):
 
     return ros_msg
 
-class Synthetic_rospkg():
+
+class SyntheticRospkg:
+    '''main node of synthetic generator'''
+
     def __init__(self):
-        rospy.init_node('Synthetic_rospkg_node')
-        rospy.loginfo('[Synthetic_rospkg_node] started')
-        rospy.Subscriber('generate_image', String, self.callback_generate_image)
-        rospy.Subscriber('start_learn', String, self.callback_start_learn)
-        try:
-            rospy.spin()
-        except:
-            rospy.signal_shutdown('Synthetic_rospkg_node is shutdown')
+        rospy.init_node("Synthetic_rospkg_node")
+        rospy.loginfo("[Synthetic_rospkg_node] started")
+        rospy.Subscriber("generate_image", String, self.callback_generate_image)
+        rospy.Subscriber("start_learn", String, self.callback_start_learn)
+        rospy.spin()
 
     def callback_generate_image(self, msg):
-        rospy.Publisher('generate_status', String, queue_size=1).publish('in progress')
-        rospy.loginfo('[Synthetic_rospkg_node] callback_generate_image called')
+        '''callback of topic [generate_image]'''
+
+        rospy.Publisher("generate_status", String, queue_size=1).publish("in progress")
+        rospy.loginfo("[Synthetic_rospkg_node] callback_generate_image called")
         count = 5 if msg.data is "" else int(msg.data)
-        project_folder_path = os.path.expanduser('~/SyntheticGenerator/' + self.get_current_project_name()) 
-        object_folder_path = project_folder_path + '/Object'
-        result_folder_path = project_folder_path + '/Result'
+        project_folder_path = os.path.expanduser(
+            "~/SyntheticGenerator/" + self.get_current_project_name()
+        )
+        object_folder_path = project_folder_path + "/Object"
+        result_folder_path = project_folder_path + "/Result"
         for i in range(count):
-            current_progress_str = "{}/{}".format(i+1, count)
-            pb = rospy.Publisher('generate_progress', String, queue_size=1)
-            pb.publish(current_progress_str)
-            rospy.loginfo('#{} : cmd'.format(i))
-            entry_file = os.path.expanduser('~/catkin_ws/src/synthetic_rospkg/vs_synthetic_generator/generate_synthetic.py')
-            cmd = "blenderproc run {} {} {}".format(
-                entry_file, object_folder_path, result_folder_path)
-            returned_value = os.system(cmd)  # returns the exit code in unix
-        rospy.Publisher('generate_status', String, queue_size=1).publish('finished')
+            current_progress_str = f"{i+1}/{count}"
+            _pb = rospy.Publisher("generate_progress", String, queue_size=1)
+            _pb.publish(current_progress_str)
+            rospy.loginfo(f"#{i} : cmd")
+            entry_file = os.path.expanduser(
+                "~/catkin_ws/src/synthetic_rospkg/vs_synthetic_generator/generate_synthetic.py"
+            )
+            cmd = f"blenderproc run {entry_file} {object_folder_path} {result_folder_path}"
+            os.system(cmd)  # returns the exit code in unix
+        rospy.Publisher("generate_status", String, queue_size=1).publish("finished")
 
     def callback_start_learn(self, msg):
-        rospy.Publisher('learn_status', String, queue_size=1).publish('in progress')
-        rospy.loginfo('[Synthetic_rospkg_node] callback_start_learn called')
+        '''callback of topic [start_learn]'''
+        _msg = msg
+        rospy.Publisher("learn_status", String, queue_size=1).publish("in progress")
+        rospy.loginfo("[Synthetic_rospkg_node] callback_start_learn called")
         for i in range(5):
-            pb = rospy.Publisher('learn_progress', String, queue_size=1)
-            pb.publish(str((i+1)*20))
+            _pb = rospy.Publisher("learn_progress", String, queue_size=1)
+            _pb.publish(str((i + 1) * 20))
             time.sleep(1)
 
         # create dummy model file
-        dummy_file_path = os.path.expanduser('~/SyntheticGenerator/' + self.get_current_project_name() + "/weight_file")
+        dummy_file_path = os.path.expanduser(
+            "~/SyntheticGenerator/" + self.get_current_project_name() + "/weight_file"
+        )
         self.create_folder_recursive(dummy_file_path)
-        with open(dummy_file_path + '/weight.pth', "w") as file:
-            file.write('this is a dummy weight file :)')
-        rospy.Publisher('learn_status', String, queue_size=1).publish('finished')
-        rospy.loginfo('[Synthetic_rospkg_node] model file is created successfully')
+        with open(dummy_file_path + "/weight.pth", "w", encoding="utf-8") as file:
+            file.write("this is a dummy weight file :)")
+        rospy.Publisher("learn_status", String, queue_size=1).publish("finished")
+        rospy.loginfo("[Synthetic_rospkg_node] model file is created successfully")
 
     def get_current_project_name(self):
-        config_file = os.path.expanduser('~/SyntheticGenerator/SG_Config.json')
+        '''callback of topic [generate_image]'''
+        config_file = os.path.expanduser("~/SyntheticGenerator/SG_Config.json")
         if not os.path.exists(config_file):
-            rospy.loginfo('[Synthetic_rospkg_node] error occurred in get_current_project_name')
-            return ''
-        with open(config_file, "r") as file:
+            rospy.loginfo(
+                "[Synthetic_rospkg_node] error occurred in get_current_project_name"
+            )
+            return ""
+        with open(config_file, "r", encoding="utf-8") as file:
             json_data = json.load(file)
-            return json_data.get("current_project", '')
+            return json_data.get("current_project", "")
 
     def create_folder_recursive(self, path):
+        '''callback of topic [generate_image]'''
         if not os.path.exists(path):
             os.makedirs(path)
 
-if __name__ == '__main__':
-    Synthetic_rospkg()
+
+if __name__ == "__main__":
+    SyntheticRospkg()
--- script/wait_capture.py
+++ script/wait_capture.py
@@ -1,29 +1,26 @@
-import blenderproc as bproc
+'''sample code which generates a simple image'''
+
 import random
-import bpy
-import cv2
-import numpy as np
 import time
 import sys
 import datetime
+import blenderproc as bproc
+import bpy
+import cv2
+import numpy as np
 
-class Wait_capture():
+class WaitCapture():
+    '''wait capture docstr'''
+
     def __init__(self, object_folder_path, result_folder_path):
         self.generate_images(object_folder_path, result_folder_path)
 
     def generate_images(self, object_folder_path, result_folder_path):
+        '''generate images docstr'''
+        _object_folder_path = object_folder_path
         bproc.init()
         self.set_light(self.get_light())
-
-        # obj_list = ['~/SyntheticGenerator/TEST3/Object/Can/Can.obj',
-                    # '~/catkin_ws/src/synthetic_rospkg/script/003_cracker_box_ycb/textured.obj',
-                    # '~/catkin_ws/src/synthetic_rospkg/script/006_mustard_bottle_ycb/textured.obj',
-                    # '~/catkin_ws/src/synthetic_rospkg/script/008_pudding_box_ycb/textured.obj',
-                    # '~/catkin_ws/src/synthetic_rospkg/script/009_gelatin_box_ycb/gelatin_box.obj']
-        #obj = bproc.loader.load_obj(random.choice(obj_list))
-
-        obj = bproc.loader.load_obj('/home/plaif/SyntheticGenerator/TEST3/Object/Can/Can.obj')
-
+        bproc.loader.load_obj('/home/plaif/SyntheticGenerator/TEST3/Object/Can/Can.obj')
         self.get_and_set_camera()
         rendered_data = bproc.renderer.render()
         color = rendered_data["colors"][0]
@@ -34,28 +31,31 @@ class Wait_capture():
         # save or send
         current_datetime = datetime.datetime.now()
         formatted_datetime = current_datetime.strftime("%Y%m%d_%H%M%S")
-        result_file = result_folder_path + "/result_{}.png".format(formatted_datetime)
-        print("[Wait_capture] Result image is generated at [{}]".format(result_file))
+        result_file = result_folder_path + f"/result_{formatted_datetime}.png"
+        print(f"[Wait_capture] Result image is generated at [{result_file}]")
 
         cv2.imwrite(result_file,color)
         bproc.utility.reset_keyframes()
 
     def get_light(self):
+        '''get bproc light'''
         # define a light and set its location and energy level
         light = bproc.types.Light()
         light.set_type("POINT")
         return light
 
     def set_light(self, light):
-        x = random.randint(-1500, 1500)/1000
-        y = random.randint(-1500, 1500)/1000
-        z = 2000/1000
+        '''set bproc light at fixed point'''
+        _x = random.randint(-1500, 1500)/1000
+        _y = random.randint(-1500, 1500)/1000
+        _z = 2000/1000
         energy =  random.randint(100, 500)
-        light.set_location([x, y, z])
+        light.set_location([_x, _y, _z])
         light.set_energy(energy)
         return light
 
     def get_and_set_camera(self):
+        '''docstr'''
         cam = bproc.camera
         cam.set_resolution(640, 480)
         focal_length = 35
@@ -65,19 +65,19 @@ class Wait_capture():
         # focus_point.set_location([0, 0, 0])
         # cam.add_depth_of_field(focus_point, fstop_value=0.25,focal_distance=0.8)
 
-        x = 0
-        y = 0
-        z =  random.uniform(600, 1400)/1000
+        _x = 0
+        _y = 0
+        _z =  random.uniform(600, 1400)/1000
         # yaw = np.pi/ random.uniform(-0.25, 0.25)
         yaw = random.uniform(-0.25, 0.25)
         roll = random.uniform(-0.05, 0.05)
         pitch = random.uniform(-0.05, 0.05)
-        cam_pose = bproc.math.build_transformation_mat([x, y, z], [roll, pitch, yaw])
+        cam_pose = bproc.math.build_transformation_mat([_x, _y, _z], [roll, pitch, yaw])
         cam.add_camera_pose(cam_pose)
 
         return cam
 
 if __name__ == '__main__':
-    object_folder_path = sys.argv[1]
-    result_folder_path = sys.argv[2]
-    Wait_capture(object_folder_path, result_folder_path)
+    OBJECT_PATH = sys.argv[1]
+    RESULT_PATH = sys.argv[2]
+    WaitCapture(OBJECT_PATH, RESULT_PATH)
